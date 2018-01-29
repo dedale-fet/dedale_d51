169d2ecb01c62d309f47d6579d79450ac3cccafb
Bruno Moraes <bruno.a.l.moraes@gmail.com>
2018-01-29 21:40:41 +0000

    Taking loop out of python to paralellise with qsub array instead.

--- scripts/2018-01-29_Dedale_make_LSST_spectroscopy.py
+++ scripts/2018-01-29_Dedale_make_LSST_spectroscopy.py
@@ -2,10 +2,11 @@ from __future__ import division, print_function
 import numpy as np
 import glob
 import os
+import sys
 import pandas as pd
 from astropy.io import fits
 
-def main(cosmossnap, lsst):
+def main(specname, lsst):
 
     if not os.path.exists("../data/lsst_cosmossnap_spec/"):
         os.mkdir("../data/lsst_cosmossnap_spec/")
@@ -13,35 +14,32 @@ def main(cosmossnap, lsst):
     # Open LSST table and get SpcExt IDs
     lsst_specIDs = pd.read_csv(lsst, usecols=["Id"]).values.flatten().tolist()
 
-    for specname in cosmossnap:
-        print("Processing %s" % os.path.basename(specname))
+    print("Processing %s" % os.path.basename(specname))
 
-        file_digit_id = int(os.path.basename(specname).split("_")[-1].split(".")[0])
+    file_digit_id = int(os.path.basename(specname).split("_")[-1].split(".")[0])
 
-        # Create new list for HDUs
-        lsst_hdus = []
-        lsst_hdus.append(fits.PrimaryHDU())
+    # Create new list for HDUs
+    lsst_hdus = []
+    lsst_hdus.append(fits.PrimaryHDU())
 
-        # Open FITS file
-        hdulist = fits.open(specname)
-        for hdu in hdulist[1:]:
-            specID = int(hdu.name)
-            if specID in lsst_specIDs:
-                lsst_hdus.append(hdu)
-        
-        new_hdulist = fits.HDUList(lsst_hdus)
+    # Open FITS file
+    hdulist = fits.open(specname)
+    for hdu in hdulist[1:]:
+        specID = int(hdu.name)
+        if specID in lsst_specIDs:
+            lsst_hdus.append(hdu)
+    
+    new_hdulist = fits.HDUList(lsst_hdus)
 
-        outname = ('../data/lsst_cosmossnap_spec/lsst_cosmossnap_spectra_%d.fits' % file_digit_id)
-        new_hdulist.writeto(outname, overwrite=True)
+    outname = ('../data/lsst_cosmossnap_spec/lsst_cosmossnap_spectra_%d.fits' % file_digit_id)
+    new_hdulist.writeto(outname, overwrite=True)
 
     return None
 
 
 if __name__ == "__main__":
 
-    cosmossnap_specfiles = glob.glob("/share/splinter/moraes/Dedale_Project/2016-06-13_simulated_catalogs/2016-09-06_euclid_run/2016-09-06_*fits")
-    cosmossnap_specfiles.sort()
-
+    specname = sys.argv[1]
     lsst_photometry_path = "../data/2017-12-06_LSST_photometry.csv"
 
-    main(cosmossnap_specfiles, lsst_photometry_path)
+    main(specname, lsst_photometry_path)
